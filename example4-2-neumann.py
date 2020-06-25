26cb51fb548d1e19f75c8b216681ad83afc134e9
Patrick <patrickdiehl@lsu.edu>
2020-06-25 16:16:26 -0500

    Add example for the Neumann boundary condition for eps=0.01

---
+++ example4-2-neumann.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# @author patrickdiehl@lsu.edu
+# @author serge.prudhomme@polymtl.ca
+# @date 10/05/2019
+import numpy as np
+import sys 
+import matplotlib.pyplot as plt
+import matplotlib
+from cycler import cycler
+
+matplotlib.rcParams["text.usetex"] = True
+matplotlib.rcParams["font.size"] = 12
+matplotlib.rcParams['text.latex.preamble'] = [
+    r'\usepackage{xfrac}']
+
+eps=0.01
+
+#############################################################################
+# Solve the system
+#############################################################################
+
+def solve(M,f):
+    return np.linalg.solve(M,f)
+
+
+#############################################################################
+# Exact solution 
+#############################################################################
+
+def exactSolution(x):
+    return x-((np.exp(-(1-x)/eps)-np.exp(-1/eps))/(1-np.exp(-1/eps)))
+
+#############################################################################
+# Loading
+#############################################################################
+
+def force(x):
+    return np.exp((x-1)/eps)/(eps*eps*(1-np.exp(-1/eps)))
+
+#############################################################################
+# Boundary value
+#############################################################################
+
+def boundary(x):
+    return 1-np.exp((x-1)/eps)/(eps*(1-np.exp(-1/eps)))
+
+#############################################################################
+# Assemble the stiffness matrix for the varibale horizon model (VHM)
+#############################################################################
+
+def VHM(n,h):
+        
+    MVHM = np.zeros([n,n])
+
+    MVHM[0][0] = 1.
+    MVHM[1][0] = -8.
+    MVHM[1][1] = 16.
+    MVHM[1][2] = -8.
+
+    for i in range(2,n-2):
+        MVHM[i][i-2] = -1.
+        MVHM[i][i-1] = -4.
+        MVHM[i][i] = 10.
+        MVHM[i][i+1] = -4.
+        MVHM[i][i+2] = -1.
+
+
+    MVHM[n-2][n-1] = -8.
+    MVHM[n-2][n-2] = 16.
+    MVHM[n-2][n-3] = -8.
+
+    MVHM[n-1][n-1] = 12.*h
+    MVHM[n-1][n-2] = -16.*h
+    MVHM[n-1][n-3] = 4.*h
+    
+    MVHM *= 1./(8.*h*h)
+    
+    return  MVHM
+
+#############################################################################
+#Computation
+#############################################################################
+print("n,h,VHM")
+markers = ['s','o','x','.']
+size= [24,48,96,96]
+
+for i in range(10,14):
+    n = np.power(2,i)
+    h = 1./n
+    nodes = n+1
+    
+    x = np.linspace(0,1.,nodes)
+    load = force(x) 
+    load[len(x)-1] = boundary(1)
+
+ 
+    u=np.linalg.solve(VHM(nodes,h),load)
+
+    shif = min(u[1:len(u)-1])
+    print(str(n)+","+str(h)+","+str(max(((u[1:len(u)-2]-exactSolution(x)[1:len(u)-2])/exactSolution(x)[1:len(u)-2]))))
+    plt.plot(x,u-exactSolution(x),label="$\delta=\sfrac{1}{2^{"+str(int(n/2))+"}}$", marker=markers[i-10], c="black",markevery=size[i-10],ls='')
+
+
+plt.title(r"Example with $\epsilon=$"+str(eps)+" Solution using VHM")
+plt.xlabel("x")
+plt.ylabel('Error in displacement')
+plt.xscale('linear')
+plt.yscale('linear')
+plt.grid()
+plt.legend()
+plt.savefig("VHM-Error-eps-"+str(eps)+"-neumann-Solution.pdf",bbox_inches='tight')
+
+
+plt.cla()
+monochrome = (cycler('color', ['k']) * cycler('linestyle',['-','--',':','-.']))
+ax = plt.gca()
+ax.set_prop_cycle(monochrome)
+
+for i in [2,5,7,14]:
+    n = np.power(2,i)
+    h = 1./n
+    nodes = n+1
+    
+    x = np.linspace(0,1.,nodes)
+    load = force(x) 
+    load[len(x)-1] = boundary(1)
+
+ 
+    u=np.linalg.solve(VHM(nodes,h),load)
+    plt.plot(x,u,label="$\delta=\sfrac{1}{2^{"+str(int(n/2))+"}}$")
+
+plt.plot(x,exactSolution(x),label="$u(x)$",color="blue",ls="-")
+
+plt.xlabel("x")
+plt.ylabel('Displacement $u$')
+plt.xscale('linear')
+plt.yscale('linear')
+plt.title(r"Convergence study with $\epsilon=$"+str(eps)+" Solution using VHM")
+plt.grid()
+plt.legend()
+plt.savefig("VHM-convergence-eps-"+str(eps)+"-neumann-Solution.pdf",bbox_inches='tight')
+
