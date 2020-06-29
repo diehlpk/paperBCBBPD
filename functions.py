5e86904919480c640525498f4d5f120ac6e2b44c
Patrick <patrickdiehl@lsu.edu>
2020-06-29 14:26:02 -0500

    Add EDM correction factor plot

--- functions.py
+++ functions.py
@@ -25,3 +25,29 @@ plt.legend()
 plt.grid()
 
 plt.savefig("functions.pdf",bbox_inches='tight')
+
+plt.clf()
+
+def kx(x):
+    k=1
+    return k/(4*(x/delta)-(3-2*np.log(x/delta))*x*x/delta/delta)
+
+def kx2(x):
+    k=1
+    return k/(4*((1-x)/delta)-(3-2*np.log((1-x)/delta))*(1-x)*(1-x)/delta/delta)
+
+n = np.power(2,4)
+h=1./n
+delta=2*h
+
+xp = np.linspace(0.01,2*h,2)
+plt.plot(xp,kx(xp),label="$\overline{\kappa}(x),\, x \in ]0,\delta]$",c="black")
+xp = np.linspace(1-2*h,0.99,2)
+plt.plot(xp,kx2(xp),label="$\overline{\kappa}(x),\, x \in [1-\delta,1]$",c="black",ls="-.")
+plt.grid()
+plt.xlabel("$x$")
+plt.ylabel(r"$\overline{\kappa}(x)$")
+plt.legend()
+plt.title("Correction Factor for the EDM method")
+plt.savefig("EDM-correction.pdf",bbox_inches='tight')
+
